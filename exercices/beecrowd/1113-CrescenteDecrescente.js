3d10197f83338eb6a6074856e33c8e4c319942d5
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-05-25 16:13:57 -0300

    update 1113

--- exercices/beecrowd/1113-CrescenteDecrescente.js
+++ exercices/beecrowd/1113-CrescenteDecrescente.js
@@ -1,10 +1,10 @@
-// var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var input = "5 4\n7 2\n3 8\n2 2";
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
 for (let i = 0; i < lines.length; i++) {
-    let a = parseInt(lines[i][0]);
-    let b = parseInt(lines[i][2]);
+    let line = lines[i].trim().split(' ');
+    let a = parseInt(line[0]);
+    let b = parseInt(line[1]);
 
     if (a === b) break;
     if (a < b) {
